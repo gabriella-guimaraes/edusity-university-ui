9fea2c32b7266d97bb1f2a5bf1b84913edd80fca
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-23 22:41:27 -0300

    feat: Programs component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/NavBar/Navbar';
 import Hero from './Components/Hero/Hero';
+import Programs from './Components/Programs/Programs';
+import Title from './Components/Title/Title';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subTitle="Our program" title="What we offer"/>
+        <Programs />
+      </div>
     </div>
   );
 }
