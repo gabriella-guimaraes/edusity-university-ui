4b3ab83e2f74584be11c366b46047bb5f1a01020
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-25 23:16:23 -0300

    feat: About Us component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './Components/NavBar/Navbar';
 import Hero from './Components/Hero/Hero';
 import Programs from './Components/Programs/Programs';
 import Title from './Components/Title/Title';
+import About from './Components/About/About';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <div className="container">
         <Title subTitle="Our program" title="What we offer"/>
         <Programs />
+        <About />
       </div>
     </div>
   );
