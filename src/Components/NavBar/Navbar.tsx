f10cff56e57e9b93129ed9756bb31bebff7a50c4
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-26 00:04:47 -0300

    feat: dark Navbar dynamic scroll

--- src/Components/NavBar/Navbar.tsx
+++ src/Components/NavBar/Navbar.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
 
 const Navbar = () => {
+
+    // adiciona a classe dark-nav quando o scroll passa do componente Hero
+    const [sticky, setSticky] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            window.scrollY > 50 ? setSticky(true) : setSticky(false); // se o tamanho do scroll for maior que 50 (Hero component) a classe dark-nav será adicionada!
+        })
+    }, []);
+
     return (
-        <nav className='container'>
+        <nav className={`container ${sticky? 'dark-nav' : ''}`} >
+            
             <img src={logo} alt='Edusity Logo' className='logo'/>
 
             <ul>
