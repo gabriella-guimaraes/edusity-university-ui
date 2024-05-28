d609dd345c2f3407c0d63f5185ea214e0b86749d
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-28 12:44:24 -0300

    feat: Navbar sections scroll

--- src/Components/NavBar/Navbar.tsx
+++ src/Components/NavBar/Navbar.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-scroll';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
 
@@ -19,12 +20,12 @@ const Navbar = () => {
             <img src={logo} alt='Edusity Logo' className='logo'/>
 
             <ul>
-                <li>Home</li>
-                <li>Program</li>
-                <li>About Us</li>
-                <li>Campus</li>
-                <li>Testimonials</li>
-                <li><button className='btn'>Contact us</button></li>
+                <li><Link to='Hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+                <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+                <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
+                <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+                <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+                <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
             </ul>
         </nav>
     )
