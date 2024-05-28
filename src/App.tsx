f80470bbeba87f2000703a473917bbbdcc8637a9
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-27 22:17:56 -0300

    feat: Contact component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Title from './Components/Title/Title';
 import About from './Components/About/About';
 import Campus from './Components/Campus/Campus';
 import Testimonials from './Components/Testimonials/Testimonials';
+import Contact from './Components/Contact/Contact';
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
         <Campus />
         <Title subTitle="Testimonials" title="What Students Says"/>
         <Testimonials />
+        <Title subTitle="Contact Us" title="Get In Touch"/>
+        <Contact />
       </div>
     </div>
   );
