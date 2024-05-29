8c334346a2ce18f72cb75afd35a75e52df716fd4
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-28 21:44:58 -0300

    feat: mobile Navbar menu

--- src/Components/NavBar/Navbar.tsx
+++ src/Components/NavBar/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
+import menu_icon from '../../assets/menu-icon.png';
 
 const Navbar = () => {
 
@@ -14,12 +15,18 @@ const Navbar = () => {
         })
     }, []);
 
+    const [mobile, setMobile] = useState(false);
+
+    const toggleMenu = () => {
+        mobile? setMobile(false) : setMobile(true);
+    }
+
     return (
         <nav className={`container ${sticky? 'dark-nav' : ''}`} >
             
             <img src={logo} alt='Edusity Logo' className='logo'/>
 
-            <ul>
+            <ul className={mobile? '' : 'hide-mobile-menu'}>
                 <li><Link to='Hero' smooth={true} offset={0} duration={500}>Home</Link></li>
                 <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
                 <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
@@ -27,6 +34,7 @@ const Navbar = () => {
                 <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
                 <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
             </ul>
+            <img src={menu_icon} alt="hamburger menu icon - mobile" className='menu-icon' onClick={toggleMenu}/>
         </nav>
     )
 }
